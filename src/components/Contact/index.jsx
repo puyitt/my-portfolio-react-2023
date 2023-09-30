66a4dbd399e3df8b7b8d2294a8a7f7a7f2360248
ThetPaingSoe <thetpaingsoe.connetis@gmail.com>
2023-09-30 18:42:50 +0630

    update contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from 'react'
-import { useRef } from 'react'
-import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faEnvelope,faMapLocation,faPhone } from '@fortawesome/free-solid-svg-icons';
+import contactme from '../../assets/images/digital-contract.svg'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const form = useRef()
 
   useEffect(() => {
     setTimeout(() => {
@@ -16,21 +14,6 @@ const Contact = () => {
     }, 3000)
   }, [])
 
-  const sendEmail = (e) => {
-    e.preventDefault()
-
-    emailjs
-      .sendForm('gmail', 'template_YeJhZkgb', form.current, 'your-token')
-      .then(
-        () => {
-          alert('Message successfully sent!')
-          window.location.reload(false)
-        },
-        () => {
-          alert('Failed to send the message, please try again')
-        }
-      )
-  }
 
   return (
     <>
@@ -49,49 +32,16 @@ const Contact = () => {
               or large projects. However, if you have any other requests or
               questions, don't hesitate to contact me using below form either.
             </p>
-            <div className="contact-form">
-              <form ref={form} onSubmit={sendEmail}>
-                <ul>
-                  <li className="half">
-                    <input placeholder="Name" type="text" name="name" required />
-                  </li>
-                  <li className="half">
-                    <input
-                      placeholder="Email"
-                      type="email"
-                      name="email"
-                      required
-                    />
-                  </li>
-                  <li>
-                    <input
-                      placeholder="Subject"
-                      type="text"
-                      name="subject"
-                      required
-                    />
-                  </li>
-                  <li>
-                    <textarea
-                      placeholder="Message"
-                      name="message"
-                      required
-                    ></textarea>
-                  </li>
-                  <li>
-                    <input type="submit" className="flat-button" value="SEND" />
-                  </li>
-                </ul>
-              </form>
-            </div>
-          </div>
-          <div className="info-wrap">
             <ul className="info-map">
-              <li><span className='icon'><FontAwesomeIcon icon={faMapLocation} color="#ffd700"/></span><span className='data'>No-100, Upper Mandalay Street,<br />Kandawlay Quarter,<br />Mingalar Taung Nyunt Township,<br />Yangon. <br /></span></li>
+              <li><span className='icon'><FontAwesomeIcon icon={faMapLocation} color="#ffd700"/></span><span className='data'>No-100, Upper Mandalay Street, Kandawlay Quarter,<br />Mingalar Taung Nyunt Township, Yangon. <br /></span></li>
               <li><span className='icon'><FontAwesomeIcon icon={faPhone} color="#ffd700"/></span><span className='data'><a className="tel" href="tel:=959960927470">+959-960927470</a><br/></span></li>
               <li><span className='icon'><FontAwesomeIcon icon={faEnvelope} color="#ffd700"/></span><span className='data'>thetpaingsoe.bece@gmail.com</span></li>
             </ul>
-           
+          </div>
+          <div className="info-wrap">
+            <figure className="contactFig">
+              <img src={contactme} alt="" />
+            </figure>
           </div>
         </div>
         
